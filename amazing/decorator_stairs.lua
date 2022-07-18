954ea50be78d51fbea760f5509fd0194614ab1a5
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2022-07-18 08:55:15 +0700

    Code clean-up

--- amazing/decorator_stairs.lua
+++ amazing/decorator_stairs.lua
@@ -13,6 +13,8 @@ function Decorator.decorate(state)
     -- add stairs up
     state.map.set(state.start.x, state.start.y, Tile.STAIR_UP)
 
+    -- determine stair down position
+    local stair_x, stair_y = nil, nil
     if state.rooms and #state.rooms > 1 then
         local room_dist = {}
 
@@ -21,28 +23,29 @@ function Decorator.decorate(state)
             local dist = getDistance(state.start.x, state.start.y, cx, cy)
             room_dist[#room_dist + 1] = { room = room, dist = dist }
         end
+
         table.sort(room_dist, function(a, b) return a.dist > b.dist end)
 
-        -- add stairs down
-        local stair_x, stair_y = room_dist[1].room.center()
-        state.map.set(stair_x, stair_y, Tile.STAIR_DN)
+        stair_x, stair_y = room_dist[1].room.center()
     else
         -- create a Dijkstra map which we'll use to calculate tile distances
         local d_map = dijkstraMap(state.map, state.start.x, state.start.y, Tile.WALL)
 
         -- find tile furthest away from start position to place stairs down
-        local stairs = { x = 0, y = 0, dist = 0 }
+        local stair_dn = { x = 0, y = 0, dist = 0 }
         for x, y, dist in d_map.iter() do
-            if dist > stairs.dist then
-                stairs = { x = x, y = y, dist = dist }
+            if dist > stair_dn.dist then
+                stair_dn = { x = x, y = y, dist = dist }
             end
         end
 
-        assert(stairs.dist > 0, 'could not place stairs down')
-
-        -- add stairs down
-        state.map.set(stairs.x, stairs.y, Tile.STAIR_DN)
+        stair_x, stair_y = stair_dn.x, stair_dn.y
     end
+
+    assert(stair_x ~= nil and stair_y ~= nil, 'could not place stair down')
+
+    -- add stairs down
+    state.map.set(stair_x, stair_y, Tile.STAIR_DN)
 end
 
 return Decorator
